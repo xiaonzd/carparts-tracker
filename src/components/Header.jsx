f40dad47aa6507004d680beebe44df8d11700590
xiaonzd <patriciapire123@gmail.com>
2026-03-27 20:47:09 +0000

    added a pointer cursor to all buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,17 +9,17 @@ export default function Header( {title} ) {
             <h2 className="title">{title}</h2>
             <div style={{ display: "flex", gap: "20px" }}>
                 {location.pathname !== "/" && (
-                    <button className="button" onClick={() => window.location.href = "/"} style={{ cursor: "pointer" }}>
+                    <button className="button orange" onClick={() => window.location.href = "/"}>
                         <BsSpeedometer2 style={icon} />Dashboard
                     </button>
                 )}
                 {location.pathname !== "/orders" && (
-                    <button className="button" onClick={() => window.location.href = "/orders"} style={{ cursor: "pointer" }}>
+                    <button className="button" onClick={() => window.location.href = "/orders"}>
                         <BsCart2 style={icon} />All Orders
                     </button>
                 )}
                 {location.pathname !== "/clients" && (
-                    <button className="button" onClick={() => window.location.href = "/clients"} style={{ cursor: "pointer" }}>
+                    <button className="button" onClick={() => window.location.href = "/clients"}>
                         <BsPeople style={icon} />All Clients
                     </button>
                 )}
