e2bc8815dd14da3e37da9e3cb369532ca0eb295f
xiaonzd <patriciapire123@gmail.com>
2026-03-27 22:13:38 +0000

    key changes

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://wpxknseeccyblaxsqvpe.supabase.co"
-const supabaseKey = "sb_publishable_XAFY4WXGPMlCALti97UsJg_Azce_Udd"
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
