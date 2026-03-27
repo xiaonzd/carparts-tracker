e64b64844f6562c1b22720c751a45a9243913d09
xiaonzd <patriciapire123@gmail.com>
2026-03-27 20:52:02 +0000

    #9

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/dashboard";
 import AllClients from "./pages/allClients";
+import AllOrders from "./pages/allOrders";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/clients" element={<AllClients />} />
+        <Route path="/orders" element={<AllOrders />} />
       </Routes>
     </BrowserRouter>
   );
