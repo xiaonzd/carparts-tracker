30bb5e26751f492532a9fa0430cb9670e639c86f
xiaonzd <patriciapire123@gmail.com>
2026-04-09 22:16:00 +0100

    #12

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/dashboard";
-import AllClients from "./pages/allClients";
-import AllOrders from "./pages/allOrders";
-import AllParts from "./pages/allParts";
+import Dashboard from "./pages/Dashboard";
+import AllClients from "./pages/AllClients";
+import AllOrders from "./pages/AllOrders";
+import AllParts from "./pages/AllParts";
 
 export default function App() {
   return (
@@ -11,6 +11,7 @@ export default function App() {
         <Route path="/" element={<Dashboard />} />
         <Route path="/clients" element={<AllClients />} />
         <Route path="/orders" element={<AllOrders />} />
+        <Route path ="/orders/:id" element={<Order />} />
         <Route path="/parts" element={<AllParts />} />
       </Routes>
     </BrowserRouter>
---
+++ src/components/cards/OrderProfileDetails.jsx
@@ -0,0 +1,7 @@
+import { useEffect, useState } from "react";
+import Card from "../Card";
+import { supabase } from "../../supabaseClient";
+
+export default function OrderProfileDetails() {
+    
+}
